1264b946e0103dc1709b0a99bd4f2bc0d7321d56
Saharia Shihab <imsahariashihab@gmail.com>
2026-06-14 03:37:30 +0600

    feat: add settings configuration for editor and workspace preferences

---
+++ src/settings.js
@@ -0,0 +1,163 @@
+export default {
+    "editor.fontFamily": "MonoLisa Variable",
+    "editor.fontLigatures": "'calt', 'liga', 'ss01', 'ss02', 'ss05', 'ss06', 'ss07', 'ss08', 'ss17'",
+    "editor.fontWeight": "normal",
+    "editor.lineHeight": 2,
+    "editor.wordWrap": "on",
+    "editor.letterSpacing": 0.4,
+    "files.autoSave": "afterDelay",
+    "files.autoSaveDelay": 100,
+    "editor.formatOnSave": true,
+    "editor.fontSize": 14,
+    "editor.lineNumbers": "off",
+    "editor.scrollbar.horizontal": "hidden",
+    "editor.scrollbar.vertical": "hidden",
+    "editor.mouseWheelZoom": true,
+    "editor.padding.top": 24,
+    "editor.smoothScrolling": true,
+    "editor.formatOnPaste": true,
+    "editor.formatOnType": true,
+    "prettier.tabWidth": 4,
+    "workbench.colorTheme": "Catppuccin Mocha",
+    "editor.suggestSelection": "first",
+    "terminal.integrated.fontSize": 14,
+    "terminal.integrated.lineHeight": 1.5,
+    "editor.cursorWidth": 2,
+    "editor.guides.bracketPairs": true,
+    "workbench.sideBar.location": "right",
+    "workbench.editor.showTabs": "single",
+    "workbench.startupEditor": "none",
+    "workbench.tips.enabled": false,
+    "workbench.tree.renderIndentGuides": "none",
+    "workbench.tree.indent": 12,
+    "explorer.compactFolders": false,
+    "editor.cursorSmoothCaretAnimation": "on",
+    "editor.cursorBlinking": "solid",
+    "editor.cursorStyle": "block",
+    "workbench.colorCustomizations": {
+        "editorCursor.background": "#f1f3f4",
+        "[Aura Dracula Spirit (Soft)]": {
+            "editorSuggestWidget.selectedBackground": "#201e27",
+            "sideBar.background": "#201b2a"
+        }
+    },
+    "editor.tokenColorCustomizations": {
+        "textMateRules": [
+            {
+                "scope": "comment",
+                "settings": {
+                    "fontStyle": "italic"
+                }
+            }
+        ]
+    },
+    "window.zoomLevel": 0.5,
+    "terminal.integrated.cursorBlinking": true,
+    "workbench.productIconTheme": "fluent-icons",
+    "workbench.editor.highlightModifiedTabs": true,
+    "terminal.integrated.copyOnSelection": true,
+    "terminal.integrated.defaultLocation": "editor",
+    "files.trimTrailingWhitespace": true,
+    "files.insertFinalNewline": true,
+    "editor.overviewRulerBorder": false,
+    "files.exclude": {
+        "**/node_modules": true
+    },
+    "editor.renderLineHighlightOnlyWhenFocus": true,
+    "workbench.editor.enablePreviewFromCodeNavigation": true,
+    "editor.minimap.enabled": false,
+    "editor.unicodeHighlight.nonBasicASCII": false,
+    "explorer.confirmDelete": false,
+    "terminal.integrated.defaultProfile.windows": "Git Bash",
+    "security.workspace.trust.untrustedFiles": "open",
+    "editor.multiCursorModifier": "ctrlCmd",
+    "prettier.useTabs": false,
+    "colorize.languages": [
+        "javascript",
+        "json"
+    ],
+    "[jsonc]": {
+        "editor.defaultFormatter": "vscode.json-language-features"
+    },
+    "[javascript]": {
+        "editor.defaultFormatter": "vscode.typescript-language-features"
+    },
+    "[html]": {
+        "editor.defaultFormatter": "vscode.html-language-features"
+    },
+    "code-runner.runInTerminal": true,
+    "[json]": {
+        "editor.defaultFormatter": "vscode.json-language-features"
+    },
+    "explorer.confirmDragAndDrop": false,
+    "window.titleBarStyle": "custom",
+    "svg.preview.mode": "img",
+    "editor.linkedEditing": true,
+    "[css]": {
+        "editor.defaultFormatter": "aeschli.vscode-css-formatter"
+    },
+    "editor.quickSuggestions": {
+        "comments": "on"
+    },
+    "files.associations": {
+        "*.js": "javascript"
+    },
+    "[typescript]": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "errorLens.enabledDiagnosticLevels": [
+        "warning",
+        "info",
+        "error"
+    ],
+    "terminal.external.windowsExec": "\"C:\\Program Files\\Git\\git-cmd.exe\"",
+    "diffEditor.hideUnchangedRegions.enabled": true,
+    "git.confirmSync": false,
+    "workbench.editor.enablePreview": false,
+    "gitlens.launchpad.indicator.enabled": false,
+    "github.copilot.nextEditSuggestions.enabled": true,
+    "material-icon-theme.hidesExplorerArrows": true,
+    "workbench.iconTheme": "material-icon-theme",
+    "workbench.navigationControl.enabled": false,
+    "chat.commandCenter.enabled": false,
+    "window.commandCenter": false,
+    "github.copilot.enable": {
+        "*": false,
+        "plaintext": false,
+        "markdown": false,
+        "scminput": false
+    },
+    "git.addAICoAuthor": "off",
+    "diffEditor.maxComputationTime": 0,
+    "workbench.statusBar.visible": false,
+    "php.validate.executablePath": "",
+    "svelte.enable-ts-plugin": true,
+    "totalTypeScript.hideAllTips": true,
+    "git.autofetch": true,
+    "json.schemaDownload.enable": true,
+    "javascript.updateImportsOnFileMove.enabled": "never",
+    "livePreview.notifyOnOpenLooseFile": false,
+    "workbench.editor.empty.hint": "hidden",
+    "keyboard.mapAltGrToCtrlAlt": true,
+    "zenMode.centerLayout": false,
+    "workbench.activityBar.location": "hidden",
+    "update.showReleaseNotes": false,
+    "diffEditor.ignoreTrimWhitespace": false,
+    "gitlens.ai.model": "vscode",
+    "gitlens.ai.vscode.model": "copilot:gpt-4.1",
+    "explorer.fileNesting.patterns": {
+        "*.ts": "${capture}.js",
+        "*.js": "${capture}.js.map, ${capture}.min.js, ${capture}.d.ts",
+        "*.jsx": "${capture}.js",
+        "*.tsx": "${capture}.ts",
+        "tsconfig.json": "tsconfig.*.json",
+        "package.json": "package-lock.json, yarn.lock, pnpm-lock.yaml, bun.lockb, bun.lock",
+        "*.sqlite": "${capture}.${extname}-*",
+        "*.db": "${capture}.${extname}-*",
+        "*.sqlite3": "${capture}.${extname}-*",
+        "*.db3": "${capture}.${extname}-*",
+        "*.sdb": "${capture}.${extname}-*",
+        "*.s3db": "${capture}.${extname}-*"
+    },
+    "C_Cpp.clang_format_style": "{ BasedOnStyle: Google, IndentWidth: 4, BreakBeforeBraces: Attach, AllowShortFunctionsOnASingleLine: All }"
+}
